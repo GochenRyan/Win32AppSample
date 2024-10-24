2d46896b18297ea29309bd2d3158aa472b6b3d30
Gaocheng <rangaocheng@outlook.com>
2024-10-24 18:27:13 +0800

    add ignore

--- Win32AppSample/HelloWin32App/HelloWin32App.cpp
+++ Win32AppSample/HelloWin32App/HelloWin32App.cpp
@@ -12,6 +12,14 @@ int Run();
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="hInstance">instance handle</param>
+/// <param name="hPrevInstance"></param>
+/// <param name="pCmdLine"></param>
+/// <param name="nCmdShow">how to show this window</param>
+/// <returns></returns>
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
 	if (!InitWindowsApp(hInstance, nCmdShow))
@@ -20,19 +28,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 	return Run();
 }
 
+
+/// <summary>
+/// Create and init main window
+/// </summary>
+/// <param name="instanceHandle">instance handle</param>
+/// <param name="show"></param>
+/// <returns></returns>
 bool InitWindowsApp(HINSTANCE instanceHandle, int show)
 {
 	WNDCLASS wc;
 
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;
+	wc.style = CS_HREDRAW | CS_VREDRAW;  // Horizontal Redraw | Virticle Redraw
+	wc.lpfnWndProc = WndProc;  // l: Long, p: Pointer, fn: Function
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = instanceHandle;
 	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
 	wc.hCursor = LoadCursor(0, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wc.lpszMenuName = 0;
+	wc.lpszMenuName = 0;  // l: Long, p: Pointer, sz: String Zero-Terminated
 	wc.lpszClassName = L"BasicWndClass";
 
 	if (!RegisterClass(&wc))
@@ -66,29 +81,56 @@ bool InitWindowsApp(HINSTANCE instanceHandle, int show)
 	return true;
 }
 
+/// <summary>
+/// Message loop
+/// | Message Queue   | -----> | GetMessage Loop | -----> | WndProc (Message   |
+/// </summary>
+/// <returns></returns>
 int Run()
 {
 	MSG msg = { 0 };
 
 	BOOL bRet = 1;
 
-	while ((bRet = GetMessage(&msg, 0, 0, 0)) != 0)
+	//while ((bRet = GetMessage(&msg, 0, 0, 0)) != 0)
+	//{
+	//	if (bRet == -1)
+	//	{
+	//		MessageBox(0, L"GetMessage FAILED", L"Error", MB_OK);
+	//		break;
+	//	}
+	//	else
+	//	{
+	//		TranslateMessage(&msg);
+	//		DispatchMessage(&msg);
+	//	}
+	//}
+
+	while (msg.message != WM_QUIT)
 	{
-		if (bRet == -1)
-		{
-			MessageBox(0, L"GetMessage FAILED", L"Error", MB_OK);
-			break;
-		}
-		else
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+		}
+		else
+		{
+			// Game logic
 		}
 	}
 
 	return (int)msg.wParam;
 }
 
+
+/// <summary>
+/// Window process
+/// </summary>
+/// <param name="hWnd"></param>
+/// <param name="msg"></param>
+/// <param name="wParam"></param>
+/// <param name="lParam"></param>
+/// <returns></returns>
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
